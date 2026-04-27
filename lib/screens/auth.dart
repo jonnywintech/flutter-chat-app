fc0d9bb70e20d12b1bf0434e4fcc76b1f35f337c
Nikola Stankovic <jonnywintech@gmail.com>
2026-04-27 15:45:05 +0200

    adding validation on input fields

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -8,6 +8,22 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>();
+
+  var _isLogin = true;
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+
+  void _submit() {
+    final isValid = _form.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    _form.currentState!.save();
+    print(_enteredEmail);
+    print(_enteredPassword);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,11 +48,15 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Padding(
                     padding: const EdgeInsets.all(16),
                     child: Form(
+                      key: _form,
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         mainAxisSize: MainAxisSize.min,
                         children: [
                           TextFormField(
+                            onSaved: (value) {
+                              _enteredEmail = value!;
+                            },
                             key: const ValueKey('email'),
                             keyboardType: TextInputType.emailAddress,
                             decoration: const InputDecoration(
@@ -44,9 +64,20 @@ class _AuthScreenState extends State<AuthScreen> {
                             ),
                             autocorrect: false,
                             textCapitalization: TextCapitalization.none,
+                            validator: (value) {
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  !value.contains('@')) {
+                                return 'Please enter a valid email address.';
+                              }
+                              return null;
+                            },
                           ),
                           const SizedBox(height: 12),
                           TextFormField(
+                            onSaved: (value) {
+                              _enteredPassword = value!;
+                            },
                             key: const ValueKey('password'),
                             obscureText: true,
                             decoration: const InputDecoration(
@@ -55,11 +86,31 @@ class _AuthScreenState extends State<AuthScreen> {
                             autocorrect: false,
                             textCapitalization: TextCapitalization.none,
                             obscuringCharacter: '*',
+                            validator: (value) {
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  value.trim().length < 6) {
+                                return 'Please enter a valid password.';
+                              }
+                              return null;
+                            },
                           ),
                           const SizedBox(height: 20),
                           ElevatedButton(
-                            onPressed: () {},
-                            child: const Text('Login'),
+                            onPressed: _submit,
+                            child: Text(_isLogin ? 'Login' : 'Sign up'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text(
+                              _isLogin
+                                  ? 'Create new account'
+                                  : 'I already have an account',
+                            ),
                           ),
                         ],
                       ),
